465a237bb8d8bfcb7f0e64c36ea6d03eb47d8cff
Alexey <slow.net@mail.ru>
2022-11-03 14:40:14 +0300

    Решение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу которая сравнивает две строки.
+// Пользователь вводит две строки. Если строки равны 
+//то выводится сообщение true если нет то false.
+// Примеры:
+//  A = "ABC" B = "ABC" => true
+//  A = "CBA" B = "ABCD" => false
+Console.WriteLine("Введите две строки");
+string? line1 = Console.ReadLine();
+string? line2 = Console.ReadLine();
+Console.WriteLine(String.Equals(line1,line2));
+
