8f4bb7e4f05b481a7639565ea939884ebf5fb06e
Alexey <slow.net@mail.ru>
2022-11-03 15:14:32 +0300

    Решение четвёртой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу которая заполняет массив рандомными числами.
+//  Количество рандомных чисел пользователь задает из консоли. 
+//  Вывести заполненый массив в консоль.
+// Пример: Вввели 3 => 7, 4, 5
+
+Console.WriteLine("Введите размер массива");
+bool isParseSize = int.TryParse(Console.ReadLine(), out int size);
+if (!isParseSize)
+{
+    Console.WriteLine("Вы ввели не корректные данные");
+}
+int[] array = Array(size);
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(0, 10);
+        array[i] = randomNumber;
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    return array;
+}
