736e6e1d8f9c6e994f840e6b04bee4d7eb8f3aca
Alexey <slow.net@mail.ru>
2022-11-09 18:24:47 +0300

    Решение 4 задачи дубль два

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -11,16 +11,15 @@ if (!isParseSize)
 }
 int[] fillInArrayRandomly = FillInArrayRandomly(size);
 PrintArray(fillInArrayRandomly);
-void PrintArray(int [] array)
+void PrintArray(int[] array)
 {
-Console.Write("[");
-for(int i = 0;i<array.Length-1;i++)
-{
-    Console.Write($"{array[i]},");
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
 }
-Console.Write($"{array[array.Length-1]}]");
-}
-
 int[] FillInArrayRandomly(int size)
 {
     int[] array = new int[size];
